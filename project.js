f8251633a48a28d3f4947bc86c5309010743d797
saqib hussain <hussainsakib44@gmail.com>
2022-02-22 10:31:42 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,9 @@
 let minutes = document.querySelector('#min');
 let seconds = document.querySelector('#sec');
-let startNPause = document.querySelector('.startNPause');
+let startButton = document.querySelector('.start');
+let pauseButton = document.querySelector('.pause');
 let session = document.querySelector('.session');
+let reduction;
 
 session.addEventListener('click', e => {
   if (e.target.matches('#increase')) {
@@ -12,5 +14,30 @@ session.addEventListener('click', e => {
   }
   if (e.target.matches('#reset')) {
     minutes.textContent = '25';
+    seconds.textContent = '00';
   }
 });
+
+function clickStart() {
+  reduction =  setInterval(() => {
+    if (seconds.textContent == 0) {
+      seconds.textContent = '59';
+      minutes.textContent -= 1;
+    }
+    else {
+      seconds.textContent -= 1;
+    }
+    }, 1000);
+  startButton.style.display = 'none';
+  pauseButton.style.display = 'inline';
+}
+
+startButton.addEventListener('click', clickStart);
+
+function clickPause() {
+  clearInterval(reduction);
+  startButton.style.display = 'inline';
+  pauseButton.style.display = 'none';
+}
+
+pauseButton.addEventListener('click', clickPause);
