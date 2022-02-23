0726bb6bf4115f27c82f849f3b33f7e331f74275
nicobreins <bharatswaroop98@gmail.com>
2022-02-23 17:20:10 +0530

    fix: negative timer and numbers below 10

--- project.js
+++ project.js
@@ -7,29 +7,35 @@ let reduction;
 
 session.addEventListener('click', e => {
   if (e.target.matches('#increase')) {
-    minutes.textContent = Number(minutes.textContent) + 1;
+    minutes.textContent = ('0' + (Number(minutes.textContent) + 1)).slice(-2);
   }
-  if (e.target.matches('#decrease')) {
-    minutes.textContent = Number(minutes.textContent) - 1;
+  if (e.target.matches('#decrease') && minutes.textContent !== '00') {
+    minutes.textContent = ('0' + (Number(minutes.textContent) - 1)).slice(-2);
   }
   if (e.target.matches('#reset')) {
     minutes.textContent = '25';
     seconds.textContent = '00';
+    clickPause();
   }
 });
 
 function clickStart() {
   reduction =  setInterval(() => {
-    if (seconds.textContent == 0) {
-      seconds.textContent = '59';
-      minutes.textContent -= 1;
-    }
-    else {
-      seconds.textContent -= 1;
-    }
+    
+      if (seconds.textContent == 0) {
+        seconds.textContent = '59';
+        minutes.textContent = ('0' + (minutes.textContent - 1)).slice(-2);
+      }
+      else{
+        seconds.textContent = ('0' + (seconds.textContent - 1)).slice(-2);
+      }
+
+      stopTimer();    
+    
     }, 1000);
   startButton.style.display = 'none';
   pauseButton.style.display = 'inline';
+  stopTimer();
 }
 
 startButton.addEventListener('click', clickStart);
@@ -38,6 +44,12 @@ function clickPause() {
   clearInterval(reduction);
   startButton.style.display = 'inline';
   pauseButton.style.display = 'none';
+}
+
+function stopTimer(){
+  if(seconds.textContent == '00' && minutes.textContent == '00'){
+    clickPause();      
+  }  
 }
 
 pauseButton.addEventListener('click', clickPause);
